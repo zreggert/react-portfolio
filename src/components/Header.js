c92b27849111dea9ae9fe671df355882ed4f1951
elgalloloco <zreggert@gmail.com>
2021-07-05 00:44:58 -0400

    added header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Zachary Eggert</h1>
+            <h3>Full Stack Web Developer</h3>
+        </header>
+    );
+}
+
+export default Header;
